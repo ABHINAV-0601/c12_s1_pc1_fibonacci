8b0c913f2d587bee361648c53dcd6be1d05d9ee0
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-10-28 12:01:03 +0530

    completed the tasks

---
+++ c12_s1_pc1_fibonacci/src/main/java/com/question/App.java
@@ -0,0 +1,15 @@
+package com.question;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Fibonacci fib = new Fibonacci();
+        System.out.print ("Fibonacci Series = "  );
+        fib.printFibonacciSeries(9);
+    }
+}
---
+++ c12_s1_pc1_fibonacci/src/main/java/com/question/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.question;
+
+public class Fibonacci {
+
+    public long fibonacci(long number) {
+
+        if(number < 0){
+            return -1;
+        }
+        else if(number <= 1){
+            return number;
+        }
+        else {
+            return fibonacci(number-1) + fibonacci(number - 2);
+        }
+    }
+    public void printFibonacciSeries(long number){
+        for (int i = 0; i < number; i++) {
+            System.out.print(" " + fibonacci(i));
+        }
+    }
+}
